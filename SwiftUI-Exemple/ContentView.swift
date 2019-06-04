3f5f71e08eeab8b1ff3d94cb6f46b149894b7466
Maria Eugênia <maria.teixeira@b2wdigital.com>
2019-06-04 10:23:00 -0300

    Initial Commit

---
+++ SwiftUI-Exemple/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUI-Exemple
+//
+//  Created by Maria Eugenia Pereira Teixeira on 04/06/19.
+//  Copyright © 2019 Maria Eugenia Pereira Teixeira. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
